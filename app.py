120bb65912914da11eacc8d9abe3cdaa9933cc16
Siddhant-Debta <128400922+Siddhant-Debta@users.noreply.github.com>
2025-06-01 11:42:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,8 @@
 import streamlit as st
 import requests
 import os
-from dotenv import load_dotenv
+api_key = st.secrets["GROQ_API_KEY"]
 
-# Load environment variables
-load_dotenv()
 BACKEND_URL = "http://127.0.0.1:5000"
 
 
